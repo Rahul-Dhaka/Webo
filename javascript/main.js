39f62017144fae89ae04c6cfea0e6d571861180d
Tarun Kumar <tarunnain467@gmail.com>
2020-07-22 22:10:15 -0800

    creating blog page

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+  
+    // Owl-Carousel for blog
+    $('.owl-carousel').owlCarousel({
+    	loop:true,
+    	autoplay:true,
+    	autoplayTimeout: 3000,
+    	dots:false,
+    	nav:true,
+    	navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')] 
+    });
+
+});
