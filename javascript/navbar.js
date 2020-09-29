3ee8f7887ece743d240883862e3f8cc3e2e42d5d
Rahul-Dhaka <dhakarahul16@gmail.com>
2020-09-29 17:25:37 +0530

    added animation in navbar

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -3,4 +3,5 @@ const navbarLinks = document.getElementsByClassName("nav-links")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
+  toggleButton.classList.toggle('move');
 });
